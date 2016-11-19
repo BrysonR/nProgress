d50ba7fd7cffbba579b872f858700e8c67df948b
bryson-reynolds <bryson@brysonreynolds.com>
2016-11-19 17:51:08 -0600

    adding shortname and leveraging regex for url pattern

--- content.js
+++ content.js
@@ -1,8 +1,18 @@
+function matchRule(str, rule) {
+  rule = rule.split("*").join(".*");
+  rule = "^" + rule + "$"
+
+  var regex = new RegExp(rule);
+
+  return regex.test(str);
+}
+
 chrome.storage.sync.get(function (data) {
     data.projects.forEach(function (project) {
-        if (project.urlPattern === window.location.href) {
-            chrome.runtime.sendMessage({msg: "capture"}, function(response, err) {
+        var isMatch = matchRule(window.location.href, project.urlPattern);
 
+        if (isMatch) {
+            chrome.runtime.sendMessage({msg: "capture"}, function(response, err) {
                 $.ajax({
                     type: "POST",
                     url: "http://📷📸📷.ws:3000/image", //xn--tu8hac.ws
